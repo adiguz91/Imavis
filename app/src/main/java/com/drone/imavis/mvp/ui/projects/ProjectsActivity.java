5de1e25c9447aa62b9ac16d64a3443aab90c9af8
adiguz91 <adiguzik@gmail.com>
2017-05-11 20:39:02 +0200

    fix projext crash

--- app/src/main/java/com/drone/imavis/mvp/ui/projects/ProjectsActivity.java
+++ app/src/main/java/com/drone/imavis/mvp/ui/projects/ProjectsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.daimajia.swipe.SwipeLayout;
 import com.drone.imavis.mvp.R;
 import com.drone.imavis.mvp.data.SyncService;
 import com.drone.imavis.mvp.ui.MainActivity;
@@ -54,6 +55,48 @@ public class ProjectsActivity  extends BaseActivity implements ProjectsMvpView {
         if (getIntent().getBooleanExtra(EXTRA_TRIGGER_SYNC_FLAG, true)) {
             //startService(SyncService.getStartIntent(this));
         }
+    }
+
+    private void loadSwipeLayout() {
+        SwipeLayout swipeLayout =  (SwipeLayout)findViewById(R.id.projectSwipeLayout);
+
+        //set show mode.
+        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+
+        //add drag edge.(If the BottomView has 'layout_gravity' attribute, this line is unnecessary)
+        swipeLayout.addDrag(SwipeLayout.DragEdge.Left, findViewById(R.id.bottom_wrapper));
+
+        swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onClose(SwipeLayout layout) {
+                //when the SurfaceView totally cover the BottomView.
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+                //you are swiping.
+            }
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                //when the BottomView totally show.
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                //when user's hand released.
+            }
+        });
     }
 
     @Override
